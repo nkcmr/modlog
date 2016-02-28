7f323b55d3d58f2b8adc297bc5389bc8c0f84e33
nick comer <nick@comer.io>
2016-02-28 11:32:53 -0500

    mix up build step to compile browserify style

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,24 @@
+'use strict'
+
 module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt)
   grunt.initConfig({
     uglify: {
       dist: {
         options: {
           sourceMap: true
         },
-        src: 'src/modlog.js',
+        src: 'dist/modlog.js',
         dest: 'dist/modlog.min.js'
+      }
+    },
+    browserify: {
+      dist: {
+        files: {
+          'dist/modlog.js': ['src/modlog.js']
+        }
       }
     }
   })
-  grunt.loadNpmTasks('grunt-contrib-uglify')
-  grunt.registerTask('default', ['uglify:dist'])
+  grunt.registerTask('default', ['browserify:dist', 'uglify:dist'])
 }
